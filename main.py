816d1c126ac5a9bf1919e4ead115adae96f18119
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2023-11-23 20:59:10 +0200

    reworked schema query

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+
 import queries
 from database import make_query
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
 
-@app.post("/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/api/sql")
 async def root(body: str = Body(media_type='text/plain')):
     res = make_query(body)
     return {"result": res}
@@ -13,8 +28,7 @@ async def root(body: str = Body(media_type='text/plain')):
 @app.get("/api/schema")
 async def get_schema():
     tables = make_query(queries.TABLES)
-    table = "car"
-    q = queries.COLUMNS % table
-    columns = make_query(q)
-    return {"tables": tables,
-            "columns": columns}
+    for table in tables:
+        columns = make_query(queries.COLUMNS % table['table_name'])
+        table['columns'] = columns
+    return tables
